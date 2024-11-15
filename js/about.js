3f716ff228d652177a376de6e51b93512fe24389
hoanganh261104 <22130012@st.hcmuaf.edu.vn>
2024-11-15 15:03:27 +0700

    Add files via upload

---
+++ js/about.js
@@ -0,0 +1,10 @@
+const memberPath = ["https://nagit09.github.io/22130015_profile/",
+    "https://doqhung.github.io/GTNM_21130365/",
+    "https://hoanganh261104.github.io/trangwebcanhan./",
+    "https://dovanminhtai1.github.io/HCL/",
+    "https://m2k1423.github.io/aboutme/"
+];
+function redirect (num){
+    
+    window.open(memberPath[num]);
+}
